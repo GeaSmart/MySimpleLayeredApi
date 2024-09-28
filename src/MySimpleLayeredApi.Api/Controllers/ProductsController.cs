f96036ec46073bafcfc30a088a3a482f029f3ec3
Gerson Azabache <gerson_dunk@hotmail.com>
2024-09-28 18:38:27 -0500

    Products get and post endpoints added, sample purposes only, not best practices applied yet

--- src/MySimpleLayeredApi.Api/Controllers/ProductsController.cs
+++ src/MySimpleLayeredApi.Api/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySimpleLayeredApi.Entities;
+using MySimpleLayeredApi.Persistence;
 
 namespace MySimpleLayeredApi.Api.Controllers
 {
@@ -6,6 +9,42 @@ namespace MySimpleLayeredApi.Api.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
 
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var data = await context.Products.ToListAsync();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            try
+            {
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocurrió un problema" + ex.Message);
+                throw;
+            }
+        }
     }
 }
